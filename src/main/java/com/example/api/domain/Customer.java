704c0dc994617514ef3ea4e5de388e65a3077862
Leonardo Henrique Pereira <leonardo.pereira@tokiomarine.com.br>
2018-12-03 19:01:34 -0200

    first commit

---
+++ src/main/java/com/example/api/domain/Customer.java
@@ -0,0 +1,51 @@
+package com.example.api.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false)
+	@NotEmpty
+	private String name;
+
+	@Column(nullable = false)
+	@NotEmpty
+	@Email
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
